1ebcf68b677e044820a9e20d5a39c89081ee5ffd
Pieter Scheffers <pieter.scheffers@gmail.com>
2015-08-05 16:04:22 +0200

    Changed url

--- src/freebsd_install.sh
+++ src/freebsd_install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Input manual to execute
-# fetch --no-verify-hostname --no-verify-peer https://www.pieterscheffers.nl/install/freebsd_install.sh
-# cd /usr/ports/ftp/wget && make install distclean && cd - && wget --no-check-certificate https://www.pieterscheffers.nl/install/freebsd_install.sh && chmod +x freebsd_install.sh && sh ./freebsd_install.sh
+# fetch --no-verify-hostname --no-verify-peer https://github.com/PieterScheffers/node-freebsd-admin/blob/master/src/freebsd_install.sh
+# cd /usr/ports/ftp/wget && make install distclean && cd - && wget --no-check-certificate https://github.com/PieterScheffers/node-freebsd-admin/blob/master/src/freebsd_install.sh && chmod +x freebsd_install.sh && sh ./freebsd_install.sh
 # TODO: replace link with github link
 
 ### To execute:
@@ -28,8 +28,8 @@ cd freebsd_admin
 npm install shelljs
 
 # Get install script (js)
-#wget --no-check-certificate https://www.pieterscheffers.nl/install/freebsd_admin.js # TODO replace with npm install
-fetch --no-verify-hostname --no-verify-peer https://www.pieterscheffers.nl/install/freebsd_admin.js
+#wget --no-check-certificate https://github.com/PieterScheffers/node-freebsd-admin/blob/master/src/freebsd-admin.js # TODO replace with npm install
+fetch --no-verify-hostname --no-verify-peer https://github.com/PieterScheffers/node-freebsd-admin/blob/master/src/freebsd-admin.js
 
 # Execute admin script
 node freebsd_admin.js
